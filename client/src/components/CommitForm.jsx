5bd946e2e0f108fa53c459231e9e8aa2834d87d6
Vignesh Shivhare <vigneshshivhare28@gmail.com>
2023-10-18 00:01:41 +0530

    Update CommitForm.jsx

--- client/src/components/CommitForm.jsx
+++ client/src/components/CommitForm.jsx
@@ -20,7 +20,7 @@ export default function CommitForm(){
     };
 
     try {
-      const response = await fetch('commit-bot-p7nz.vercel.app/user/commit', {
+      const response = await fetch('https://commit-bot-p7nz.vercel.app/user/commit', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
